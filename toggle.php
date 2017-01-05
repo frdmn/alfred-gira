6b694ea10f6c9f1cc7488fcaea2d8ed5a1fdbc95
Jonas Friedmann <j@frd.mn>
2017-01-05 10:57:53 +0100

    Use giracli instead of reinvented wheel

--- toggle.php
+++ toggle.php
@@ -1,20 +1,9 @@
 #!/usr/bin/env php
 <?php
-
 // Load configuration
-$config = @json_decode(@file_get_contents($_SERVER['HOME'].'/.alfred-gira.json'));
 
-// For each configured outlet in configuration ...
-foreach ($config->outlets as $outlet) {
-    // Read desired state from argv
-    $state = (strtolower($argv[1]) == 'on' ? 'ein' : 'aus');
-    // Control outlet using HTTP/curl
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $config->server.'/'.$config->office.'licht0'.$outlet.$state);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-}
+putenv("PATH=" .@$_ENV["PATH"]. ':'.$_SERVER['HOME'].'/.npm-packages/bin:/usr/local/bin');
+shell_exec("gira {$argv[1]}");
 
 // Exit without errors
 exit;
